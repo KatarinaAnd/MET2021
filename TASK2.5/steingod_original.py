9755ac1719ffd8b062a46f5605bfc8f6fbb2ca98
Katarina Andersen <56952321+KatarinaAnd@users.noreply.github.com>
2021-07-30 12:30:35 +0200

    Add files via upload
    Updatet 30.07

--- TASK2.5/steingod_original.py
+++ TASK2.5/steingod_original.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# import packages
 import os
 import sys
 import argparse
@@ -17,6 +18,8 @@ from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 import pprint
 
+# function for applying arguments in the command line
+# python <filename>.py -c <config-file>.cfg -s <start date> -e <end date>
 def parse_arguments():
     parser = argparse.ArgumentParser()
     
@@ -43,14 +46,15 @@ def parse_arguments():
 
     return args
 
+# function for reading the configuration file
 def parse_cfg(cfgfile):
-    # Read config file
     print("Reading", cfgfile)
     with open(cfgfile, 'r') as ymlfile:
         cfgstr = yaml.full_load(ymlfile)
 
     return cfgstr
 
+# function creating a log-file
 def initialise_logger(outputfile = './log'):
     # Check that logfile exists
     logdir = os.path.dirname(outputfile)
@@ -80,6 +84,8 @@ def initialise_logger(outputfile = './log'):
 
     return(mylog)
 
+
+# function that gets the list of ships in data storage.
 def getships(frostcfg):
 
     myrequest = 'municipality=skip'
@@ -102,16 +108,8 @@ def getships(frostcfg):
 
 args = parse_arguments()
 cfgstr = parse_cfg(args.cfgfile)
-#r = requests.get(cfgstr['endpointmeta'],
-#                'municipality=skip',
-#                auth=(cfgstr['client_id'],""))
-#print(r)
-#print(cfgstr['frostcfg']['endpointmeta'])
-#print(cfgstr)
-#getships(cfgstr)
-#getships(cfgstr['frostcfg'])
-#print(cfgstr['frostcfg']['endpointmeta'])
-#sys.exit()
+
+# Function for making the data to a netcdf
 
 def extractdata(frostcfg,station,stmd,output):
 
@@ -124,49 +122,43 @@ def extractdata(frostcfg,station,stmd,output):
                 auth=(frostcfg['client_id'],""))
     except:
         mylog.error('Something went wrong extracting metadata')
+
     # Check if the request worked, print out any errors
     if not r.ok:
         mylog.error('Returned status code was %s with response %s', r.status_code, r.text)
         raise
     metadata = json.loads(r.text)
+
     # Connect and read information on available parameters at the station
     mylog.info('Retrieving parameters for station: %s', station)
-    myrequest = 'sources='+ station+'&elements='+','.join(frostcfg['elements'])
+    myrequest = 'sources='+ station+'&elements='+','.join(frostcfg['elements']) + ", longitude, latitude"
     try:
         r = requests.get(frostcfg['endpointparameters'],
                 myrequest,
                 auth=(frostcfg['client_id'],""))
     except:
         mylog.error('something wen wrong extracting metadata.')
+        raise
+
     # Check if the request worked, print out any errors
     if not r.ok:
         mylog.error('Returned status code was %s, message was:\n%s', r.status_code, r.text)
         raise
+
+
     parameterlist = json.loads(r.text)
-    #pprint.pprint(parameterlist)
-    #sys.exit()
-    # Create list of parameters to extract, latitude and olongitude are automatically added. Only checking for observations, i.e. data within hours and minutes, not daily or monthly aggregates.
-    myparameters = ''
-    i = 0
-    avvars = []
-    for item in parameterlist['data']:
-        if 'PT' in item['timeResolution']:
-            print(item['timeResolution']+' - '+item['elementId'])
-            avvars.append(item['elementId'])
-            if i == 0:
-                myparameters = item['elementId']
-            else:
-                myparameters += ','+item['elementId']
-            i+=1 
-    print(myparameters)
+
     # Create request for observations
+    # note that longitude and latitude always is added
+
     mylog.info('Retrieving data for station: %s', station)
     myrequest = {
             'sources': station,
-            'elements': myparameters,
+            'elements': ','.join(frostcfg['elements'])  + ', longitude, latitude',
             'fields': ','.join(frostcfg['fields']),
             'referencetime': '/'.join([args.startday,args.endday])
             }
+    
     # Connect and read observations
     try:
         r = requests.get(frostcfg['endpointobs'],
@@ -175,6 +167,7 @@ def extractdata(frostcfg,station,stmd,output):
     except:
         mylog.error('Something went wrong extracting data.')
         raise
+
     # Check if the request worked, print out any errors
     if r.status_code == 412:
         mylog.error('Information returned indicates that no data is available for this time period for station %s', station)
@@ -182,95 +175,116 @@ def extractdata(frostcfg,station,stmd,output):
     if not r.status_code == 200:
         mylog.error('Returned status code was %s\nmessage:\n%s', r.status_code, r.text)
         raise
+    
     # Read into  Pandas DataFrame, assuming - is used for missing values.
     df = pd.read_csv(StringIO(r.text),header=0,
         mangle_dupe_cols=True, parse_dates=['referenceTime'],
         index_col=False,na_values=['-'])
+    
+    # remove unwated symbols in variabel name
+    df.columns = df.columns.str.rstrip('\(-\)')
 
-    columnnames = list(df.columns)
-    columnnames = [s.replace('(-)','') for s in list(df.columns)]
-    columnnames[columnnames.index('referenceTime')] = 'time'
-    df.columns = columnnames
-
-    timemin = min(df['time'])
-    timemax = max(df['time'])
+    # extracting information on the time frame, both for the filename and netcdf content
+    timemin = min(df['referenceTime'])
+    timemax = max(df['referenceTime'])
     datasetstart = timemin.strftime('%Y-%m-%dT%H:%M:%SZ')
     datasetend = timemax.strftime('%Y-%m-%dT%H:%M:%SZ')
     datasetstart4filename = timemin.strftime('%Y%m%d')
     datasetend4filename = timemax.strftime('%Y%m%d')
-    mytimes = (pd.to_datetime(df['time'], utc=True)-pd.Timestamp("1970-01-01", tz='UTC')) // pd.Timedelta('1s')
-    # Make sure time is dimension and coordinate variable
-    ds_timeseries = df.set_index(['time']).to_xarray()
-    # Make sure metadata are correct for variables
-    #pprint.pprint(parameterlist)
-    #print(type(parameterlist))
-    #print(parameterlist['data'])
-##    avvars = []
-##    for item in parameterlist['data']:
-##        avvars.append(item['elementId'])
-    print("#################")
-    for item in ds_timeseries.data_vars.keys():
+
+    # converting time and adding it as index
+    mytimes = (pd.to_datetime(df['referenceTime'], utc=True)-pd.Timestamp("1970-01-01", tz='UTC')) // pd.Timedelta('1s')
+    df['time'] = mytimes
+    df = df.set_index('time')
+    
+    # creating a list of elements that we want in our df/netcdf
+    elements = frostcfg['elements'][0].split(',')
+    elements = [x.rstrip(', ').lstrip(' ').lower() for x in elements]
+    elements.append('longitude')
+    elements.append('latitude')
+    df = df[elements].copy()
+
+
+    # creating netcdf file from df
+    # expanding the dims to add lon and lat
+    ds_station = xr.Dataset.from_dataframe(df)
+    ds_station = ds_station.expand_dims(dim = {'lon': ds_station.longitude.values, 'lat': ds_station.latitude.values})
+
+    ds_station.time.attrs['standard_name'] = 'time'
+    ds_station.time.attrs['units'] = 'seconds since 1970-01-01 00:00:00+0'
+    ds_station.lon.attrs['standard_name'] = 'longitude'
+    ds_station.lon.attrs['units'] = 'degrees'
+    ds_station.lat.attrs['standard_name'] = 'latitude'
+    ds_station.lat.attrs['units'] = 'degrees'
+
+    # saving lat lon values for global attributes
+    lat_min = ds_station.latitude.values.min()
+    lat_max = ds_station.latitude.values.max()
+    lon_min = ds_station.longitude.values.min()
+    lon_max = ds_station.longitude.values.max()
+
+    # removing longitude and latitude as data variables
+    ds_station = ds_station.drop_vars(['longitude','latitude'])
+
+
+    avvars = []
+    for item in parameterlist['data']:
+        avvars.append(item['elementId'])
+    
+    for item in ds_station.data_vars.keys():
         if item in avvars:
+            varname = item
+            print(varname)
             for myel in parameterlist['data']:
                 if item in myel['elementId']:
-                    ds_timeseries.data_vars[item].attrs['standard_name'] = myel['elementId']
-                    ds_timeseries.data_vars[item].attrs['standard_name'] = myel['unit']
-                    ds_timeseries.data_vars[item].attrs['units'] = myel['unit']
-                    #ds_timeseries['data'][0]['_FillValue'] = '-'
+                    ds_station[item].attrs['standard_name'] = myel['elementId']
+                    ds_station[item].attrs['units'] = myel['unit']
+                    ds_station[item].attrs['long_name'] = myel['elementId'].replace('_', ' ')
                     break
-    print('Now I have come here...')
-    print(ds_timeseries)
-    sys.exit()
 
     # Need to convert from dataarray to dataset in order to add global attributes
-    ds_timeseries.attrs['featureType'] = 'timeSeries'
-    ds_timeseries.attrs['title'] = 'Weather station information from ship '+stmd['name']
-    ds_timeseries.attrs['summary'] = output['abstract']
-    ds_timeseries.attrs['license'] = metadata['license']
-    ds_timeseries.attrs['time_coverage_start'] = datasetstart
-    ds_timeseries.attrs['time_coverage_end'] = datasetend
-    ds_timeseries.attrs['geospatial_lat_min'] = min(ds_timeseries.data_vars['latitude'].values)
-    ds_timeseries.attrs['geospatial_lat_max'] = max(ds_timeseries.data_vars['latitude'].values)
-    ds_timeseries.attrs['geospatial_lon_min'] = min(ds_timeseries.data_vars['longitude'].values)
-    ds_timeseries.attrs['geospatial_lon_max'] = max(ds_timeseries.data_vars['longitude'].values)
-    ds_timeseries.attrs['creator_name'] = stmd['PrincipalInvestigator'] 
-    ds_timeseries.attrs['creator_email'] = stmd['PrincipalInvestigatorEmail']
-    ds_timeseries.attrs['creator_url'] = stmd['PrincipalInvestigatorOrganisationURL']
-    ds_timeseries.attrs['creator_institution'] = stmd['PrincipalInvestigatorOrganisation']
+    ds_station.attrs['featureType'] = 'timeSeries'
+    ds_station.attrs['title'] = 'Weather station information from ship '+stmd['name']
+    ds_station.attrs['summary'] = output['abstract']
+    ds_station.attrs['license'] = metadata['license']
+    ds_station.attrs['time_coverage_start'] = datasetstart
+    ds_station.attrs['time_coverage_end'] = datasetend
+    ds_station.attrs['geospatial_lat_min'] = lat_min
+    ds_station.attrs['geospatial_lat_max'] = lat_max
+    ds_station.attrs['geospatial_lon_min'] = lon_min
+    ds_station.attrs['geospatial_lon_max'] = lon_max
+    ds_station.attrs['creator_name'] = stmd['PrincipalInvestigator'] 
+    ds_station.attrs['creator_email'] = stmd['PrincipalInvestigatorEmail']
+    ds_station.attrs['creator_url'] = stmd['PrincipalInvestigatorOrganisationURL']
+    ds_station.attrs['creator_institution'] = stmd['PrincipalInvestigatorOrganisation']
+    
+
     # Remember to fix these FIXME
-    ds_timeseries.attrs['keywords'] = 'Earth Science > Cryosphere > Frozen Ground > Permafrost > Permafrost Temperature,Earth Science > Land Surface > Soils > Soil temperature'
-    ds_timeseries.attrs['keywords_vocabulary'] = 'GCMD'
-    ds_timeseries.attrs['publisher_name'] = ''
-    ds_timeseries.attrs['publisher_email'] = 'adc@met.no'
-    ds_timeseries.attrs['publisher_url'] = 'https://adc.met.no/'
-    ds_timeseries.attrs['publisher_institution'] = 'Norwegian Meteorlogical Institute'
-    ds_timeseries.attrs['Conventions'] = 'ACDD, CF-1.8'
-    ds_timeseries.attrs['date_created'] = metadata['createdAt']
-    ds_timeseries.attrs['history'] = metadata['createdAt']+': Data extracted from the MET Observation Database through Frost and stored as NetCDF-CF'
-    ds_timeseries.attrs['source'] = 'Soil temperature from permafrost boreholes'
-    ds_timeseries.attrs['wigosId'] = metadata['data'][0]['wigosId']
-    ds_timeseries.attrs['METNOId'] =  station
-    ds_timeseries.attrs['project'] = stmd['Project']
-
-    ds_timeseries.encoding['unlimited_dims'] = 'time'
-
-    pprint.pprint(metadata['data'])
-    print(metadata['data'][0]['name'])
-    print(metadata['data'][0]['stationHolders'])
-    print(ds_timeseries)
+    #ds_timeseries.attrs['keywords'] = 'Earth Science > Cryosphere > Frozen Ground > Permafrost > Permafrost Temperature,Earth Science > Land Surface > Soils > Soil temperature'
+    ds_station.attrs['keywords_vocabulary'] = 'GCMD'
+    ds_station.attrs['publisher_name'] = ''
+    ds_station.attrs['publisher_email'] = 'adc@met.no'
+    ds_station.attrs['publisher_url'] = 'https://adc.met.no/'
+    ds_station.attrs['publisher_institution'] = 'Norwegian Meteorlogical Institute'
+    ds_station.attrs['Conventions'] = 'ACDD, CF-1.8'
+    ds_station.attrs['date_created'] = metadata['createdAt']
+    ds_station.attrs['history'] = metadata['createdAt']+': Data extracted from the MET Observation Database through Frost and stored as NetCDF-CF'
+    ds_station.attrs['source'] = 'Soil temperature from permafrost boreholes'
+    ds_station.attrs['wigosId'] = metadata['data'][0]['wigosId']
+    ds_station.attrs['METNOId'] =  station
+    ds_station.attrs['project'] = stmd['Project']
+    
+    print(ds_station)
 
     # Dump to Netcdf
     outputfile = output['destdir']+'/ship-'+metadata['data'][0]['wigosId']+'_'+datasetstart4filename+'-'+datasetend4filename+'.nc'
     mylog.info('Dumping data to NetCDF-CF:\n%s', outputfile)
     try:
-        ds_timeseries.to_netcdf(outputfile)
+        ds_station.to_netcdf(outputfile)
     except:
         mylog.error('Creation of NetCDF file didn\'t work properly\n%s', sys.exc_info()[0])
         raise
-##            encoding={'depth': {'dtype':'int32'},
-##                'time': {'dtype': 'int32'},
-##                'soil_temperature': {'dtype': 'float32'}
-##                })
+
     return
 
 if __name__ == '__main__':
@@ -300,28 +314,8 @@ if __name__ == '__main__':
     # Loop through ships
     mylog.info('Processing ships from the list.')
 
-#print(cfgstr['stations']['SN97710'])
-#print('------------')
-#j = cfgstr['stations'].items()
-#print(j)
-#print()   
-    #for item in ships['data']:
-    #    print(item)
-        #print(cfgstr['stations']['SN15270'])
-        #print(cfgstr['stations'][item['id']])
-        #print('----------')
-    #   #print(cfgstr['stations'])
-        #mylog.info('Extracting information for %s - %s',item['name'], item['id'])
-        #try:
-        #    extractdata(cfgstr['frostcfg'], 'SN97710', cfgstr['stations']['SN97710'], cfgstr['output'])
-        #except:
-        #    mylog.error('Something went horrible wrong here.')
-        #    raise SystemExit()
-    #print(cfgstr['stations'].items())
-   #print('----------')
+
     for station,content in cfgstr['stations'].items():
-        print(station)
-        print(content)
         if station in ['SN99927']:
             continue
         mylog.info('Requesting data for: %s', station)
